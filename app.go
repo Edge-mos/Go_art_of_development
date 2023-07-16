4b9c8342dfdce6ebbc5c355fb855fc85600319fa
Vladimir Yamnikov <Androedge@gmail.com>
2023-07-16 23:24:14 +0300

    loops basics

--- app.go
+++ app.go
@@ -3,5 +3,20 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World")
+
+	//standard
+	var sum = 0
+	for i := 0; i < 10; i++ {
+		sum += i
+	}
+	fmt.Println(sum)
+
+	//while analog
+	var sum2 = 0
+	for sum2 < 1000 {
+		sum2 += 10
+	}
+
+	fmt.Println(sum2)
+
 }
