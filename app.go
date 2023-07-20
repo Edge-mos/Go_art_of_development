a1186a24aa40d67acd48ca5dad2b3f5dfc3b8191
Vladimir Yamnikov <Androedge@gmail.com>
2023-07-20 03:27:30 +0300

    defer complete

--- app.go
+++ app.go
@@ -3,5 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World")
+	fmt.Println("Defer")
+
+	defer fmt.Println(1)
+	defer fmt.Println(2)
+	defer fmt.Println(3)
+
+	fmt.Println("defer is stack - LIFO")
 }
