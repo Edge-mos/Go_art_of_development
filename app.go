aba8ddd7f781518f89335680620f2e6e44835224
Vladimir Yamnikov <Androedge@gmail.com>
2023-07-25 02:09:13 +0300

    pointers tutorial added

--- app.go
+++ app.go
@@ -3,5 +3,16 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World")
+	fmt.Println("Pointers in Go")
+
+	var variable = "Hello World"
+	var pointer = &variable
+
+	fmt.Println(variable)                              // print value
+	fmt.Printf("Pointer : %v \n", pointer)             // print pointer in hex-format (16)
+	fmt.Printf("Pointer dereference: %v \n", *pointer) // dereference - get value thru pointer
+
+	*pointer = "oh my!"
+	fmt.Printf("New value thru pointer : %v \n", variable)
+
 }
