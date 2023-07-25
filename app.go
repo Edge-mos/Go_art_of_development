62c226cd311a4f4c8e8919aad6d27aed8b227565
Vladimir Yamnikov <Androedge@gmail.com>
2023-07-25 03:20:54 +0300

    arrays added

--- app.go
+++ app.go
@@ -3,5 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World")
+	fmt.Println("Arrays")
+
+	var arr [2]string
+	arr[0] = "hello"
+	arr[1] = "world"
+
+	fmt.Println(arr)
+
+	var numbers = []int{1, 2, 3}
+
+	fmt.Println(numbers)
 }
