609e46b78db7ac0a88c1ee8f84565ba036393923
Vladimir Yamnikov <Androedge@gmail.com>
2023-08-07 00:35:59 +0300

    error beginner added

--- app.go
+++ app.go
@@ -3,5 +3,37 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World")
+	fmt.Println("Error handling")
+
+	err := throwError()
+	if err != nil {
+		fmt.Println(err)
+	}
+
 }
+
+//type Error interface {
+//	Error() string
+//}
+
+type AppError struct {
+	Err    error
+	Custom string
+	Field  int
+}
+
+func (ae *AppError) Error() string {
+	fmt.Println(ae.Custom)
+	return ae.Err.Error()
+}
+
+func throwError() error {
+	appError := AppError{
+		Err:    fmt.Errorf("my error"),
+		Custom: "value here",
+		Field:  89,
+	}
+	return &appError
+}
+
+//var _ Error = &AppError{}
