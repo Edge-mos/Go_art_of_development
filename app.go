f6e7a7f5a0afcc7e1979346fcef57f944df3b813
Vladimir Yamnikov <Androedge@gmail.com>
2023-08-09 03:01:29 +0300

    reader interface as practice

--- app.go
+++ app.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"strings"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	fmt.Println("Reader interface")
+
+	reader := strings.NewReader("Hello World")
+	sls := make([]byte, 8)
+
+	fmt.Printf("initial slice: %v \n", sls)
+
+	for {
+		n, err := reader.Read(sls)
+		fmt.Printf("n = %d err = %v sls = %v ", n, err, sls[:n])
+		fmt.Printf("sls --> %q \n", sls[:n])
+
+		if err == io.EOF {
+			break
+		}
+	}
+
+	fmt.Println(sls)
+
 }
